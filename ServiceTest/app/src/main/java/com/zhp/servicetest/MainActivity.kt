10032d63c3ae3f545632219985e3149a2975b956
Jimzhang <1061449734@qq.com>
2024-12-02 11:00:54 +0800

    xxx

---
+++ ServiceTest/app/src/main/java/com/zhp/servicetest/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.zhp.servicetest
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Bundle
+import android.os.IBinder
+import androidx.appcompat.app.AppCompatActivity
+import com.zhp.servicetest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var downloadBinder: MyService.DownloadBinder
+    private val connection = object : ServiceConnection {
+        // onServiceConnected会在Activity和Service成功绑定之后调用
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            downloadBinder = service as MyService.DownloadBinder
+            downloadBinder.startDownload()
+            downloadBinder.getProgress()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startServiceBtn.setOnClickListener {
+            val intent = Intent(this, MyService::class.java)
+            startService(intent) // 启动Service
+        }
+        binding.stopServiceBtn.setOnClickListener {
+            val intent = Intent(this, MyService::class.java)
+            stopService(intent) // 停止Service
+        }
+
+        binding.bindServiceBtn.setOnClickListener {
+            val intent = Intent(this, MyService::class.java)
+            bindService(intent, connection, Context.BIND_AUTO_CREATE) // 绑定Service
+        }
+        binding.unbindServiceBtn.setOnClickListener {
+            unbindService(connection) // 解绑Service
+        }
+    }
+}
