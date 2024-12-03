f3b725d4498d546c9cde19606f7c39cf19eabf52
Jimzhang <1061449734@qq.com>
2024-12-03 23:56:20 +0800

    drawerlayou

---
+++ MaterialTest/app/src/main/java/com/zhp/materialtest/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.zhp.materialtest
+
+import android.os.Bundle
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import com.zhp.materialtest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.backup -> Toast.makeText(this, "You clicked Backup",
+                Toast.LENGTH_SHORT).show()
+            R.id.delete -> Toast.makeText(this, "You clicked Delete",
+                Toast.LENGTH_SHORT).show()
+            R.id.settings -> Toast.makeText(this, "You clicked Settings",
+                Toast.LENGTH_SHORT).show()
+            android.R.id.home -> binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return true
+    }
+}
